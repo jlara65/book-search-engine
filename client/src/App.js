e60e85fb846c99d0c3136b314229280bcc5765da
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-12-04 15:42:07 -0600

    Created addUser mutation and implemented with signupForm.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   InMemoryCache,
   createHttpLink,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -14,9 +15,19 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 // establish apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,21 @@
 import gql from '@apollo/client';
+
+// quires for logged in users
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
